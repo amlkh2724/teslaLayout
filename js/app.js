4f06c40558313d44cb79c323096f4b3b39e6e53b
amal khanjer <amlkh2724@gmail.com>
2022-12-30 19:57:30 +0200

    added more css timer

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
-function speedConverter(valNum) {
-    valNum = parseFloat(valNum);
-    document.getElementById("outputKPH").innerHTML = valNum * 1.609344;
-  }
+
+function updateTimer() {
+  var timer = document.getElementById("timer");
+  timer.innerHTML = parseInt(timer.innerHTML) + 1;
+}
+
+var timerInterval = setInterval(updateTimer, 200);
+
+setTimeout(function() {
+  clearInterval(timerInterval);
+}, 600);
+
+
